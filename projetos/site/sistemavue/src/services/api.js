c9dd3857dd47d8d3727b77e5700696afdaee98df
Tamires01-alt <62455956+Tamires01-alt@users.noreply.github.com>
2023-09-19 00:55:29 -0300

    Delete projetos/site/sistemavue directory

--- projetos/site/sistemavue/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create ({
-    baseURL: 'https://pokeapi.co/api/v2', 
-    timeout: 3000, 
-    headers: {'X-Custom-Header' : 'foobar'}
-}); 
-
-export default api 

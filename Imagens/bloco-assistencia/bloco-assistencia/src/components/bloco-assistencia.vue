1531c54f94377ad4cea20dddf13f2a209f2869a0
“Tamires01-alt” <rtamires117@gmail.com>
2023-09-19 00:27:26 -0300

    iniciando repositorio

---
+++ Imagens/bloco-assistencia/bloco-assistencia/src/components/bloco-assistencia.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1> hjdashgjdsahjhjgsdaghjdsa </h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
